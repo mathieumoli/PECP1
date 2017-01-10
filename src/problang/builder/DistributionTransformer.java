5da23a93b394c896a2baa2c711aefb9a91886325
Loryn FONTAINE <fl407746@etu.unice.fr>
2017-01-10 22:05:54 +0100

    Changement noms des fonctions + essai de Main

--- src/problang/builder/DistributionTransformer.java
+++ src/problang/builder/DistributionTransformer.java
@@ -6,7 +6,8 @@ import problang.elems.*;
  * Created by lorynf on 09/01/17.
  */
 public final class DistributionTransformer {
-    public static Distribution distribute(Distribution d) {
+
+    public static Distribution transformation(Distribution d) {
         // La distribution finale à remplir
         Distribution d1 = new Distribution();
 
@@ -15,48 +16,52 @@ public final class DistributionTransformer {
             // On récupère le programme
             Program p = c.getProgram();
 
-            // On crée le programme qu'on aura après l'affectation (le reste du programme)
-            Program p1 = new Program(p.getCommands().subList(1, p.getCommands().size()));
-
-            // On récupère l'état
-            State s = c.getState();
-
             // Si la première commande est une affectation
             if (p.getCommand(0).affectation() != null) {
-                //TODO mettre cette partie dans une autre méthode
-                String var = p.getCommand(0).affectation().var().IDENT().getText();
-
-                // Premier cas : on affecte une expression
-                if (p.getCommand(0).affectation().expr() != null) {
-                    int value;
-                    // Si c'est un nombre, on récupère la valeur
-                    if (p.getCommand(0).affectation().expr().NUMBER() != null) {
-                        value = Integer.parseInt(p.getCommand(0).affectation().expr().NUMBER().getText());
-                    }
-                    // Si c'est une variable, il faut récupérer la valeur dans la mémoire
-                    else {
-                        assert p.getCommand(0).affectation().expr().var() != null;
-                        //TODO
-                        value = 0;
-                    }
-                    // Si il y a une opération...
-                    if (p.getCommand(0).affectation().expr().op() != null) {
-                        //TODO
-                        value = 0;
-                    }
-                    // TODO verifier que var n'est pas déjà dedans
-                    s.getMemory().put(var, value);
-
-                    // TODO si on a déjà cette configuration additionner la probabilité
-                    d1.getElements().put(new Configuration(p1, s), 1.0 * d1.getElements().get(c));
-                }
-                // Deuxième cas : on affecte une fonction probabiliste
-                else {
-                    assert p.getCommand(0).affectation().probFunc() != null;
-                    //TODO
-                }
+                d1 = applyAffectationRule(c, d1);
             }
             // TODO les autres regles
+        }
+        return d1;
+    }
+
+    private static Distribution applyAffectationRule(Configuration c, Distribution d1) {
+        Program p = c.getProgram();
+        State s = c.getState();
+
+        // On crée le programme qu'on aura après l'affectation (le reste du programme)
+        Program p1 = new Program(p.getCommands().subList(1, p.getCommands().size()));
+
+        String var = p.getCommand(0).affectation().var().IDENT().getText();
+
+        // Premier cas : on affecte une expression
+        if (p.getCommand(0).affectation().expr() != null) {
+            int value;
+            // Si c'est un nombre, on récupère la valeur
+            if (p.getCommand(0).affectation().expr().NUMBER() != null) {
+                value = Integer.parseInt(p.getCommand(0).affectation().expr().NUMBER().getText());
+            }
+            // Si c'est une variable, il faut récupérer la valeur dans la mémoire
+            else {
+                assert p.getCommand(0).affectation().expr().var() != null;
+                //TODO
+                value = 0;
+            }
+            // Si il y a une opération...
+            if (p.getCommand(0).affectation().expr().op() != null) {
+                //TODO
+                value = 0;
+            }
+            // TODO verifier que var n'est pas déjà dedans
+            s.getMemory().put(var, value);
+
+            // TODO si on a déjà cette configuration additionner la probabilité
+            d1.getElements().put(new Configuration(p1, s), 1.0 * d1.getElements().get(c));
+        }
+        // Deuxième cas : on affecte une fonction probabiliste
+        else {
+            assert p.getCommand(0).affectation().probFunc() != null;
+            //TODO
         }
         return d1;
     }
---
+++ src/problang/builder/Main.java
@@ -0,0 +1,40 @@
+package problang.builder;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import problang.elems.Configuration;
+import problang.elems.Distribution;
+import problang.elems.Program;
+import problang.elems.State;
+import problang.grammar.ProbabilisticLanguageLexer;
+import problang.grammar.ProbabilisticLanguageParser;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by lorynf on 10/01/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // initialiser le lexer et le parser
+            ANTLRInputStream in = new ANTLRInputStream(new FileReader("Projet1/affectation"));
+            ProbabilisticLanguageLexer lexer = new ProbabilisticLanguageLexer(in);
+            BufferedTokenStream tokens = new CommonTokenStream(lexer);
+            ProbabilisticLanguageParser parser = new ProbabilisticLanguageParser(tokens);
+
+            Program initialProgram = new Program(parser.program());
+            State initialState = new State(); //TODO Il va falloir le faire mieux que ça haha
+            Configuration initialConfiguration = new Configuration(initialProgram, initialState);
+            Distribution initialDistribution = new Distribution();
+            initialDistribution.getElements().put(initialConfiguration,1.0);
+
+            DistributionTransformer.transformation(initialDistribution);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
