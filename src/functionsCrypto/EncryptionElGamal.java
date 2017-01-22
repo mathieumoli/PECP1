e924556281148e4a1e6eddd1f77c78b4d77f4281
hananeCh <hachtt@gmail.com>
2017-01-22 18:25:19 +0100

    Génèrateur Clés, Encrypt, Decrypt, ElGamal

---
+++ src/functionsCrypto/EncryptionElGamal.java
@@ -0,0 +1,42 @@
+package functionsCrypto;
+
+import java.math.BigInteger;
+
+public class EncryptionElGamal {
+	private PublicKey pk;
+	private String m;
+	
+	private BigInteger c1;
+	private BigInteger c2;
+	
+	public EncryptionElGamal(PublicKey pk, String ptext){
+		this.pk = pk;
+		m = ptext;
+	}
+	
+	public void encrypter(){
+		BigInteger g = pk.getg();
+		BigInteger q = pk.getq();
+		BigInteger ke = pk.getke();
+		BigInteger mq = new BigInteger(m.getBytes());
+		//System.out.println(mq.toString());
+		BigInteger r = GenerationCles.randomBigInteger(q.subtract(BigInteger.ONE));
+		c1 = g.modPow(r, q);
+		c2 = mq.multiply(ke.modPow(r, q)).mod(q);		
+		
+	}
+
+	public BigInteger getC1() {
+		return c1;
+	}
+
+
+	public BigInteger getC2() {
+		return c2;
+	}
+
+
+	
+	
+
+}
---
+++ src/functionsCrypto/PublicKey.java
@@ -0,0 +1,30 @@
+package functionsCrypto;
+
+import java.math.BigInteger;
+
+public class PublicKey {
+    private BigInteger q;
+    private BigInteger ke;
+    private BigInteger g;
+    
+    public PublicKey(BigInteger q,BigInteger ke,BigInteger g){
+        this.q= q;
+        this.ke=ke;
+        this.g=g;
+      }
+
+       
+       public BigInteger getq() {
+           return q;
+       }
+
+      
+       public BigInteger getke() {
+           return ke;
+       }
+
+       
+       public BigInteger getg() {
+           return g;
+       }
+}
